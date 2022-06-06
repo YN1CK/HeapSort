aa7d11127e8fd0b7f05b927f26be8eed823714f8
YN1CK <yannick.reiss@gmx.de>
2022-06-06 20:21:49 +0200

    added comments in string.h

--- include/stringops.h
+++ include/stringops.h
@@ -5,8 +5,16 @@
 #include <stdio.h>
 #include <time.h>
 
+
+// generates a c-string by counting to x with letters
+// n = length of the string
+// x = seed/target
+// returns pointer to generated string
 char* generateLinearString(int n, unsigned long x);
 
+// generates pseudo-random lowercase c-string
+// length = length of the string
+// returns pointer to generated string
 char* generateRandomString(int length);
 
 #endif//STRINGOPS_H
